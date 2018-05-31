49d59270bd1653588b8e2d02595aa5cf7592b03f
Yi An Lin <lois.kiro@gmail.com>
2018-05-31 15:56:04 +0800

    加入專案檔案。

---
+++ WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //isRock.LineBot.Bot bot = new isRock.LineBot.Bot("CVfzZeskAfcQeyevyc7k4bBONPU3oCtT0oydYm9MrDXKYK2oHC0KtjGF1wAjogrhrKM42B3Fu7DWHFNzEC9rDliPH09/3uAkIDXarQpSMACeX8fupBUpZXamhLdWsO+/PaQFpBoWHSYhRYXK6DmNUAdB04t89/1O/w1cDnyilFU=");
+            //bot.PushMessage("U26dd87213fa1f3d11d0a0a4d57a54cb5",1);
+
+        }
+    }
+}
---
+++ WebApplication1/default.aspx.cs
@@ -0,0 +1,33 @@
+using isRock.LineBot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        const string channelAccessToken = "CVfzZeskAfcQeyevyc7k4bBONPU3oCtT0oydYm9MrDXKYK2oHC0KtjGF1wAjogrhrKM42B3Fu7DWHFNzEC9rDliPH09/3uAkIDXarQpSMACeX8fupBUpZXamhLdWsO+/PaQFpBoWHSYhRYXK6DmNUAdB04t89/1O/w1cDnyilFU=";
+        const string AdminUserId= "U26dd87213fa1f3d11d0a0a4d57a54cb5";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            var bot = new Bot(channelAccessToken);
+            bot.PushMessage(AdminUserId, $"測試 {DateTime.Now.ToString()} ! ");
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            var bot = new Bot(channelAccessToken);
+            bot.PushMessage(AdminUserId, 1,2);
+        }
+    }
+}
