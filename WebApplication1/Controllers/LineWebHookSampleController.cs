06e927296b6ddcab77de959789de7a2739776b0a
Yi An Lin <lois.kiro@gmail.com>
2018-06-07 15:06:22 +0800

    測試

--- WebApplication1/Controllers/LineWebHookSampleController.cs
+++ WebApplication1/Controllers/LineWebHookSampleController.cs
@@ -35,23 +35,21 @@ namespace WebApplication1.Controllers
                 {
                     //this.ReplyMessage(LineEvent.replyToken,"我可以回答任何問題~" + LineEvent.message.text+ userInfo);
                     if (LineEvent.message.type == "text")//收到文字 
-                    {       if (LineEvent.message.text.Contains("hello"))//== "hello") //
+                    {
+                        if (LineEvent.message.text.Contains("hello"))//== "hello") //
                             this.ReplyMessage(LineEvent.replyToken, userInfo.displayName + "今天天氣不錯");
+
+                        if (LineEvent.message.text.Contains("雨"))
+                            this.ReplyMessage(LineEvent.replyToken, userInfo.displayName + "記得拿傘");
                         else
                         {
                             this.ReplyMessage(LineEvent.replyToken, "?");
                         }
-                    if (LineEvent.message.text.Contains("雨"))
-                        this.ReplyMessage(LineEvent.replyToken, userInfo.displayName + "記得拿傘");
-
-                } else
-                {
-                    this.ReplyMessage(LineEvent.replyToken, "?");
-                }
-
-
-
-
+                    }
+                    else
+                    {
+                        this.ReplyMessage(LineEvent.replyToken, "怎麼了?");
+                    }
                     if (LineEvent.message.type == "sticker") //收到貼圖
                         this.ReplyMessage(LineEvent.replyToken, 1, 2);
 
